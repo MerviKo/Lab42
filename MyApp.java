d197e55c9a2ef315edc2df494a66db26916e51cc
Mervi Koiste <t6kome00@students.oamk.fi>
2017-10-04 21:44:07 +0300

    Added changeBalance method

--- MyApp.java
+++ MyApp.java
@@ -22,4 +22,6 @@ public class main {
 	public void printProduct() {
 		System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 	}
+	public changeBalance(int newvalue)
+	{}
 }
